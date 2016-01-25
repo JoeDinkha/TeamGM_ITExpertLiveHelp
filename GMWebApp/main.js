cbb49900e153fe3f5c8df3c1596bfb1ca2aa19f7
Jenna <sanocki1@msu.edu>
2016-01-25 17:10:27 -0500

    Added availability toggle

--- GMWebApp/main.js
+++ GMWebApp/main.js
@@ -1,3 +1,22 @@
-//$(document).ready {
-//
-//}
+$(document).ready( function() {
+
+    console.log('jgkr');
+
+    $('#availabilityToggle').toggles({
+        drag: true, // allow dragging the toggle between positions
+        click: true, // allow clicking on the toggle
+        text: {
+            on: 'Online', // text for the ON position
+            off: 'Offline' // and off
+        },
+        on: false, // is the toggle ON on init
+        animate: 250, // animation time (ms)
+        easing: 'swing', // animation transition easing function
+        checkbox: null, // the checkbox to toggle (for use in forms)
+        clicker: null, // element that can be clicked on to toggle. removes binding from the toggle itself (use nesting)
+        width: 150, // width used if not set in css
+        height: 40, // height if not set in css
+        type: 'compact' // if this is set to 'select' then the select style toggle will be used
+    });
+
+});
