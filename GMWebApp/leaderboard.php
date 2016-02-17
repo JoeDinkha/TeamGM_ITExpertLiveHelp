479cb53cd78aa951f84beddcd635096b8279d404
priceja7 <Bubba944>
2016-02-17 18:14:42 -0500

    updated database and leaderboard

---
+++ GMWebApp/leaderboard.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Author: Jacob Price
+ */
+session_start();
+
+if( !isset($_SESSION['user']) ){
+    header('Location: login.php');
+}
+
+else{
+    $username = $_SESSION['user'];
+
+    //server details
+    $serverName = "35.9.22.109, 1433";
+
+    //more server details
+    $connectionInfo = array("Database" => "db", "UID" => "priceja7", "PWD" => "teamgm16");
+
+    //establish connection to database
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    //Retrieving current values in the database given a user
+    if (isset($username)){
+        //update database with given username and availability
+        $query_string = "SELECT Username, AverageRating FROM dbo.MockTable1 ORDER BY AverageRating DESC";
+
+        //run the query and store results for future use
+        $results = sqlsrv_query($conn,$query_string);
+
+        //initialize arrays to hold usernames and average ratings
+        $usernames = array();
+        $average_ratings = array();
+
+        //push each username,average_rating pair into database
+        while ($row = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC)){
+            array_push($usernames,$row['Username']);
+            array_push($average_ratings,$row['AverageRating']);
+        }
+
+
+        $results = sqlsrv_fetch_array($results,SQLSRV_FETCH_ASSOC);
+
+
+        //grabbing the average rating of the user and displaying them throughout the page
+
+        $avg_rating = $results['AverageRating'];
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GM | IT Expert Live Help - Leaderboard</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="style.css" type="text/css" />
+
+    <!-- Script Imports -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="leaderboard.js" type="text/javascript"></script>
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="favicons/favicon.ico">
+    <link rel="icon" sizes="16x16 32x32 64x64" href="favicons/favicon.ico">
+    <link rel="icon" type="image/png" sizes="196x196" href="favicons/favicon-192.png">
+    <link rel="icon" type="image/png" sizes="160x160" href="favicons/favicon-160.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="favicons/favicon-64.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16.png">
+    <link rel="apple-touch-icon" href="favicons/favicon-57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicons/favicon-114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicons/favicon-72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicons/favicon-144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicons/favicon-60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicons/favicon-120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicons/favicon-76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicons/favicon-152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/favicon-180.png">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileImage" content="favicons/favicon-144.png">
+    <meta name="msapplication-config" content="favicons/browserconfig.xml">
+</head>
+
+
+<body>
+    <div class="container">
+        <header>
+            <img src="images/Logo_of_General_Motors.png" width="2000" height="1989" alt="General Motors Logo" />
+            <h1>IT Expert Live Help - Leaderboard</h1>
+        </header>
+
+
+        <div class="content" id="leaderboard">
+            <h2>Leaderboard</h2>
+
+            <table>
+                <tr>
+                    <th>Rank</th>
+                    <th>Username</th>
+                    <th>Rating</th>
+                </tr>
+
+
+                <?php
+
+                    //php script to write our query results to the page
+                    for ($x=1; $x<=count($usernames); $x++){
+                        echo '<tr>';
+                        echo '<td>'.$x.'</td>';
+                        echo '<td>'.$usernames[$x-1].'</td>';
+                        echo '<td>'.$average_ratings[$x-1].'</td>';
+                        echo '</tr>';
+                    }
+                ?>
+
+            </table>
+        </div>
+
+
+        <footer>
+            <p>&copy; Team GM - Spring 2016</p>
+        </footer>
+    </div>
+</body>
+</html>
