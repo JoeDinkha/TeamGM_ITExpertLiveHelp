d75adacbb44e191927a47e904e0883cd723cb2a1
Jenna <sanocki1@msu.edu>
2016-04-12 02:11:39 -0400

    Working on Refresh Token PHP implementation - running into SSL problem...

---
+++ GMWebApp/refresh_api.php
@@ -0,0 +1,53 @@
+<?php
+
+$refresh_data = '[
+            "client_id": "df5c3f43-84b2-4444-a0e8-3022d364f53b",
+            "scope": "openid https://outlook.office.com/calendars.read offline_access",
+            "refresh_token": "MCYWfBo3Q7kHPdGaOTJMfHwLbiD47gvqevwB4i4rqSl8ZyTqw6S2*kpdVtv6R7O1qV95yB0kBtmmxxxrXVnyAeKn2bNJ7DGFkcXlAqfbYfuenm08m7UGUgpNtIo5KhTA7LHGxU6dqBpuVtY7vDQkrlBrLHFCTUHTAP6Mtz*hSo7IddyaWcFvgGh44XqFNKGivqtt6kMtgCnB*1RRpKaV5Abe23tFCiyXGd66dn0DVHeYIBkysby6pyqimeV7aAIX4mhqAy3kJOx2hG80i!NbMxl6iHXVsHF9CxgVoIJR7SlD7fN3gPouCV!S4eLKxYdhL8T8mTspthqXXviSYVlUnCmI6hjS6UFXtuZUIJLPx8IXJFvT3V1WVrZUZJR3C7OYctMxxlSjKblilaOAMUl1g8hx2fdHaN4eD5Xlp3tWqp46f",
+            "redirect_uri": "https://35.9.22.109/GMWebApp/index.php",
+            "grant_type": "refresh_token", "client_secret": "APfpVLBgOLKhNrD7W1SpuXR}"
+	     ]';
+
+//$raw_data = json_encode($refresh_data);
+
+//function debug_to_console( $data ) {
+//
+//    if ( $data  )
+//        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+//    else
+//        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+//
+//    echo $output;
+//}
+
+//debug_to_console($raw_data);
+//echo $new_data . '\n\n';
+//var_dump($new_data);
+
+$url = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+$content = json_encode($refresh_data);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER,
+    array("Content-type: application/json"));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+$json_response = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+if ( $status != 201 ) {
+    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+}
+
+
+curl_close($curl);
+
+$response = json_decode($json_response, true);
+
+
+echo $response;
+?>
