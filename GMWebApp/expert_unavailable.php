dba75d79c8a76452397105fe557140197eac6be4
Jenna <sanocki1@msu.edu>
2016-04-22 16:27:47 -0400

    Emails user to let them know they are overridden to offline

--- GMWebApp/expert_unavailable.php
+++ GMWebApp/expert_unavailable.php
@@ -20,6 +20,32 @@ $query_string = "UPDATE dbo.Mocktable1 SET Availability='0' WHERE SkypeName=".$e
 
 $results = sqlsrv_query($conn,$query_string);
 
+// Change this to "update the status of the user with this name to Offline"
+$query_string2 = "SELECT * FROM dbo.Mocktable1 WHERE SkypeName=".$expert;
+
+$results = sqlsrv_query($conn,$query_string);
+
+$results2 = sqlsrv_query($conn, $query_string2);
+$results3 = sqlsrv_fetch_array($results2, SQLSRV_FETCH_ASSOC);
+
+
+//Email information
+$admin_email = "noreplyGMITLiveHelp@gmail.com";
+$email = $results3['Email'];
+$name = $results3['FullName'];
+$subject = "IT Expert Live Help: Availability Notice";
+$comment = "Dear ".$name.",\n
+You were marked unavailable when a user tried to reach you for help.
+Your office hours have been overridden and you are now set to offline.\n
+Thanks, \n
+GM IT Expert Live Help
+";
+
+//send email
+mail($email, "$subject", $comment, "From:" . $admin_email);
+
+
 echo($query_string);
+echo ($results3['Email']);
 ?>
 
